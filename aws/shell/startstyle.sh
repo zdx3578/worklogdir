a46ac1b1678d67100730e11da18eb0007b3b6731
zhangdexiang <179363811@qq.com>
2016-03-19 15:19:53 +0800

    v production

--- aws/shell/startstyle.sh
+++ aws/shell/startstyle.sh
@@ -3,8 +3,8 @@ if [ ! $3 ]
 then echo 'cmd $1=source(dir/pic) $2=style(pic) $3=iterations' 
 else
 
-#root='/opt/practice/neural-style.me.fork/data/'
-root='/Users/lmj/develop/neural-style.me.fork/date/'
+root='/opt/practice/neural-style.me.fork/data/'
+#root='/Users/lmj/develop/neural-style.me.fork/date/'
 
 
 #dir prepare
@@ -23,11 +23,11 @@ source=$1
 style=$2
 iterations=$3
 
-#programepath='/opt/practice/neural-style.me.fork'
-programepath='/Users/lmj/develop/neural-style.me.fork/'
+programepath='/opt/practice/neural-style.me.fork'
+#programepath='/Users/lmj/develop/neural-style.me.fork/'
 cd $programepath
 
-nohup python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$1.$2.$3.jpg       --checkpoint-iterations 1 --checkpoint-output $root'outck/'$1.$2.%s.jpg   --iterations $3  &
+nohup python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$1.$2.$3.jpg       --checkpoint-iterations 10 --checkpoint-output $root'outck/'$1.$2.%s.jpg   --iterations $3  &
 
 pwd
 tail -f nohup.out
