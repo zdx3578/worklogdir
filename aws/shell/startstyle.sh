9ced23d0619ec8a7f9ac16af8df1639faa874fa4
lmj <lmj@lmjdeMacBook-Air.local>
2016-03-23 13:47:13 +0800

    cklog dir

--- aws/shell/startstyle.sh
+++ aws/shell/startstyle.sh
@@ -1,6 +1,4 @@
 
-#!!!!!root user
-
 if [ ! $3 ]
 then echo 'cmd $1=source(dir/pic) $2=style(pic) $3=iterations' 
 else
@@ -8,22 +6,20 @@ else
 root='/opt/practice/neural-style.me.fork/data/'
 #root='/Users/lmj/develop/neural-style.me.fork/date/'
 
-
 #dir prepare
 user=`echo $1 |awk -F'/' '{print $1}'`
 #echo $user
-mkdir -p  $root'style'
-mkdir -p  $root'out/'$user
-mkdir -p  $root'outck/'$user
-mkdir -p  $root'source/'$user
-
-
-
 #$1= source dir and file  man/picname.jpg
 source=$1
 #$2= style file
 style=$2
 iterations=$3
+
+mkdir -p  $root'style'
+mkdir -p  $root'out/'$user
+mkdir -p  $root'outck/'$user
+mkdir -p  $root'source/'$user
+
 
 programepath='/opt/practice/neural-style.me.fork'
 #programepath='/Users/lmj/develop/neural-style.me.fork/'
@@ -31,7 +27,8 @@ cd $programepath
 
 #ldconfig /usr/local/cuda/lib64
 
-nohup python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$1.$2.$3.jpg   --print-iterations 30   --checkpoint-iterations 30 --checkpoint-output $root'outck/'$1.$2.%s.jpg   --iterations $3  &  
+#nohup 
+python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$user/$1.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$2/$1.$2.%s.jpg   --iterations $3  #&  
 
 pwd
 tail -f nohup.out
