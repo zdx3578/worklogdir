e75f8457b1a02cbd957484c72e7fc72034e3c374
lmj <lmj@lmjdeMacBook-Air.local>
2016-03-23 15:57:58 +0800

    initimage2

--- aws/shell/startstyle.sh
+++ aws/shell/startstyle.sh
@@ -27,12 +27,17 @@ cd $programepath
 #ldconfig /usr/local/cuda/lib64
 
 
-#echo "  python neural_style.py --content $root'source/'$user/$userpic   --styles $root'style/'$2   --output $root'out/'$user/$userpic.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$userpic/$2/$userpic.$2.%s.jpg   --iterations $3  "
+echo "  python neural_style.py --content $root'source/'$user/$userpic   --styles $root'style/'$2   --output $root'out/'$user/$userpic.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$userpic/$2/$userpic.$2.%s.jpg   --iterations $3  --initial $root'source/'$1  "
 
 rm nohup.out
 #nohup 
 python neural_style.py --content $root'source/'$user/$userpic   --styles $root'style/'$2   --output $root'out/'$user/$userpic.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$userpic/$2/$userpic.$2.%s.jpg   --iterations $3  --initial $root'source/'$1 #&  
 
+
+#python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$1.$2.$3.jpg   --print-iterations 30   --checkpoint-iterations 30 --checkpoint-output $root'outck/'$1.$2.%s.jpg   --iterations $3
+
+date
+echo `date`
 pwd
 #tail -f nohup.out
 fi
