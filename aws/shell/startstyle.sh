657dbc72644c056a5ab77bd1f73ea50a7d0818fa
lmj <lmj@lmjdeMacBook-Air.local>
2016-03-23 21:57:45 +0800

    opt dir

--- aws/shell/startstyle.sh
+++ aws/shell/startstyle.sh
@@ -3,8 +3,8 @@ if [ ! $3 ]
 then echo 'cmd $1=source(dir/pic) $2=style(pic) $3=iterations' 
 else
 
-#root='/opt/practice/neural-style.me.fork/data/'
-root='/Users/lmj/develop/neural-style.me.fork/date/'
+root='/opt/practice/neural-style.me.fork/data/'
+#root='/Users/lmj/develop/neural-style.me.fork/date/'
 
 #dir prepare
 user=`echo $1 |awk -F'/' '{print $1}'`
@@ -20,8 +20,8 @@ mkdir -p  $root'outck/'$user/$userpic/$2
 mkdir -p  $root'source/'$user
 
 
-#programepath='/opt/practice/neural-style.me.fork'
-programepath='/Users/lmj/develop/neural-style.me.fork/'
+programepath='/opt/practice/neural-style.me.fork'
+#programepath='/Users/lmj/develop/neural-style.me.fork/'
 cd $programepath
 
 #ldconfig /usr/local/cuda/lib64
