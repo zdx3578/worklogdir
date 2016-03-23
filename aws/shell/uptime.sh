1c65521ab441dd2b401fd14aaae83df013d64de3
lmj <lmj@lmjdeMacBook-Air.local>
2016-03-23 18:33:45 +0800

    uptime 2

--- aws/shell/uptime.sh
+++ aws/shell/uptime.sh
@@ -35,21 +35,22 @@ einfo 'uptime day ' $uptd
 
 if [ `expr  $cpu \>  0.03 = 1 ]
 then  einfo 'if panduan, cpu gt 0.01;next exit ' $cpu  exit
-else  einfo 'if panduan, cpu lt 0.01 ' 
+else  einfo 'if panduan, cpu lt 0.01 ' $cpu
     if [ $mem -lt 600 ]
     then        einfo "mem $mem" "lt 600; next  exit"
     		exit
     else einfo "mem $mem" "gt 600";
 	if  [ $uptm -gt 52 ]
-	then   
+	then einfo 'uptim gt 52' $uptm  
 		if [$uptm -lt 55]
-		then
+		then einfo 'uptim lt 55'  $uptm
 		echo gt1 $upt >> /tmp/uptime
 		uptime|wall
 		echo `date` '----------after 49 sec ; poweroff'|wall
 		sleep  50
 		/sbin/poweroff -fp
 		fi
+	else einfo 'uptime lt 52'  $uptm
 	fi
     fi
 fi
