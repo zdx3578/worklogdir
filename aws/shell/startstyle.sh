7a1b37fce0d8b590a0b86335ae88786a3bbc44a8
lmj <lmj@lmjdeMacBook-Air.local>
2016-03-24 14:04:13 +0800

    --init image

--- aws/shell/startstyle.sh
+++ aws/shell/startstyle.sh
@@ -31,7 +31,7 @@ echo "  python neural_style.py --content $root'source/'$user/$userpic   --styles
 
 rm nohup.out
 #nohup 
-python neural_style.py --content $root'source/'$user/$userpic   --styles $root'style/'$2   --output $root'out/'$user/$userpic.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$userpic/$2/$userpic.$2.%s.jpg   --iterations $3  --initial $root'source/'$1 #&  
+python neural_style.py --content $root'source/'$user/$userpic   --styles $root'style/'$2   --output $root'out/'$user/$userpic.$2.$3.jpg   --print-iterations 20   --checkpoint-iterations 20 --checkpoint-output $root'outck'/$user/$userpic/$2/$userpic.$2.%s.jpg   --iterations $3  #  --initial $root'source/'$1 #&  
 
 
 #python neural_style.py --content $root'source/'$1   --styles $root'style/'$2   --output $root'out/'$1.$2.$3.jpg   --print-iterations 30   --checkpoint-iterations 30 --checkpoint-output $root'outck/'$1.$2.%s.jpg   --iterations $3
