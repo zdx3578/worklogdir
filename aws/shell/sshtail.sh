d98bbbbd5ef755be1d471dfd3211a0aca0ac3c93
lmj <lmj@lmjdeMacBook-Air.local>
2016-06-10 11:43:43 +0800

    fix

--- aws/shell/sshtail.sh
+++ aws/shell/sshtail.sh
@@ -3,16 +3,16 @@
 
 echo -------------standup 1000
 ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@a.dpl.ren 'tail work/modular_rl/nohup.out -n 1000 |grep RewP ' "
-
+echo;echo;
 
 echo -------------standup 620 7
 ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 1000 |grep RewP ' "
-
+echo;echo;
 
 
 echo -------------walk4---run
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 500| grep -A2 EpLenMe ;echo;echo;sleep 2 ; tail work/modular_rl/nohup.out -n 600| grep RewP  ' "|grep -vE "Ma|--"
-
+ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'tail work/modular_rl/nohup.out -n 600| grep RewP;  sleep 2; echo;echo; tail work/modular_rl-2p/nohup.out -n 500| grep -A2 EpLenMe ;  ' "|grep -vE "Ma|--"
+echo;echo;
 #echo -------------standup 620 7
 #ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 1000 |grep RewP ' "
 
