a70ecd88fe5e860446ae451b49fda609cd413326
lmj <lmj@lmjdeMacBook-Air.local>
2016-06-10 20:49:09 +0800

    rsync mp4

--- aws/shell/rsyncabc.sh
+++ aws/shell/rsyncabc.sh
@@ -4,18 +4,18 @@ echo;echo --------1
 #rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup5/.dir/ /Users/lmj/Documents/trainout/mjk/modelup5.c/  #&
 echo;
 echo --------2
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@a.dpl.ren:/home/ubuntu/work/out/modleup2/.dir/ /Users/lmj/Documents/trainout/mjk/modelup2-server16/  #&
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@a.dpl.ren:/home/ubuntu/work/out/modleup2/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup2-server16/  #&
 echo;
 echo --------3
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup7/.dir/ /Users/lmj/Documents/trainout/mjk/modelup7.c/  #&
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup7/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup7.c/  #&
 echo; echo --------4
 
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@b.dpl.ren:/home/ubuntu/work/out/modlewalk1/.dir/ /Users/lmj/Documents/trainout/mjk/modelwalk2/ #&
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@b.dpl.ren:/home/ubuntu/work/out/modlewalk1/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelwalk2/ #&
 echo; echo --------5
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@b.dpl.ren:/home/ubuntu/work/out/model.model.walk4/.dir/ /Users/lmj/Documents/trainout/mjk/model.model.walk4/  #&
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@b.dpl.ren:/home/ubuntu/work/out/model.model.walk4/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/model.model.walk4/  #&
 
 echo; echo --------6
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@e.dpl.ren:/home/ubuntu/work/out/model.model.walk6/.dir/ /Users/lmj/Documents/trainout/mjk/model.model.walk6/  #&
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@e.dpl.ren:/home/ubuntu/work/out/model.model.walk6/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/model.model.walk6/  #&
  
 echo; echo --------7
 #rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@d.dpl.ren:/home/ubuntu/work/out/model.model.walk5/.dir/ /Users/lmj/Documents/trainout/mjk/model.model.walk5/ #&
