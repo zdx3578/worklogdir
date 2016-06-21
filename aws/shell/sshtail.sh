f048b9da9261d3758f345a0a62581d2cd91e39d8
lmj <lmj@lmjdeMacBook-Air.local>
2016-06-21 10:52:04 +0800

    export docker

--- aws/shell/sshtail.sh
+++ aws/shell/sshtail.sh
@@ -2,24 +2,32 @@
 
 
 echo -------------standup 1000 a
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@a.dpl.ren 'tail work/modular_rl/nohup.out -n 5000 |grep RewP ;  tail work/modular_rl/nohup.out -n 60 |grep Ite  ; ps -ef|grep python|grep -v grep  ' "
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@a.dpl.ren 'tail work/modular_rl/nohup.out -n 5000 |grep RewP ;  tail work/modular_rl/nohup.out -n 90 |grep Ite  ; ps -ef|grep python|grep -v grep  ' "
 echo;echo;
 
 echo -------------standup 620 7  c
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep RewP ;  tail work/modular_rl-2p/nohup.out -n 60 |grep Ite  ; ps -ef|grep python|grep -v grep  ' "
+ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite  ; ps -ef|grep python|grep -v grep  '  "          |grep -vE  "Ma|^--"
+
 echo;echo;
 
 
-echo -------------walk4---run  b
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'echo; tail work/modular_rl-2p/nohup.out -n 5000| grep RewP;  tail work/modular_rl-2p/nohup.out -n 60 |grep Ite   ;  ps -ef|grep python|grep -v grep   ' "
+#echo -------------walk4---run  b
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'echo; tail work/modular_rl-2p/nohup.out -n 5000| grep RewP;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ;  ps -ef|grep python|grep -v grep   ' "
 echo;echo;
 
-echo -------------walk 6 e
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@e.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep RewP ;  tail work/modular_rl-2p/nohup.out -n 60 |grep Ite   ; ps -ef|grep python|grep -v grep ' "
+echo -------------stand   b
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;   tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep ' "     |grep -vE  "Ma|^--" 
 
 
 echo --------------walk 8 d 
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@d.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep RewP  ;  tail work/modular_rl-2p/nohup.out -n 60 |grep Ite   ; ps -ef|grep python|grep -v grep  ' " 
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@d.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep RewP  ;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep  ' "         
+
+
+echo -------------walk stand  e
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren 
+
+#ssh -i /Users/lmj/digits.pem ubuntu@e.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;   tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep '      |grep -vE  "Ma|^--"
+
 
 
 
