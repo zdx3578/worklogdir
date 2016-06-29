147bed8ad159edf62160ddbb3f1ce7c8899c4c9b
lmj <lmj@lmjdeMacBook-Air.local>
2016-06-29 17:58:14 +0800

    pip update

--- aws/shell/rsyncabc.sh
+++ aws/shell/rsyncabc.sh
@@ -4,10 +4,14 @@ echo;echo --------1 a
 #rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup5/.dir/ /Users/lmj/Documents/trainout/mjk/modelup5.c/  #&
 echo;
 echo --------2 a
-#rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@a.dpl.ren:/home/ubuntu/work/out/modleup2/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup2-server16/  &
+#rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@184.72.222.101:/home/ubuntu/work/out/modelup12/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup12/  &
 echo; 
 echo --------3 c
-rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup11/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup11.c/  &
+
+rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@54.210.120.4:/home/ubuntu/work/rllab/data/local/trpo-expl/trpo-expl_2016_06_29_01_56_06_0001/gym_log/*.mp4 /Users/lmj/Documents/trainout/mjk/modelupvime4/ &
+
+
+#rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@c.dpl.ren:/home/ubuntu/work/out/modleup11/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelup11.c/  &
 echo; echo --------4
 
 #rsync -e 'ssh -i  /Users/lmj/digits.pem ' -avz  ubuntu@b.dpl.ren:/home/ubuntu/work/out/modlewalk1/.dir/*.mp4 /Users/lmj/Documents/trainout/mjk/modelwalk2/ &
--- aws/shell/sshtail.sh
+++ aws/shell/sshtail.sh
@@ -6,7 +6,7 @@ echo -------------standup 1000 a
 echo;echo;
 
 echo -------------standup 620 7  c
-ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite  ; ps -ef|grep python|grep -v grep  '  "          |grep -vE  "Ma|^--"
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@c.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite  ; ps -ef|grep python|grep -v grep  '  "          |grep -vE  "Ma|^--"
 
 echo;echo;
 
@@ -16,10 +16,16 @@ echo;echo;
 echo;echo;
 
 echo -------------stand   b
-#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@b.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;   tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep ' "     |grep -vE  "Ma|^--" 
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@184.72.222.101 'tail work/modular_rl-2p/nohup.out -n 5000 |grep -A2 EpLenMe ;   tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep ' "     |grep -vE  "Ma|^--" 
 
 
 echo --------------walk 8 d 
+
+#ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@184.72.222.101 'tail work/modular_rl-2p.2/nohup.out -n 5000 |grep -A2 EpLenMe ;   tail work/modular_rl-2p.2/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep ' "     |grep -vE  "Ma|^--"
+
+
+ssh -i /Users/lmj/digits.pem ubuntu@54.210.120.4 'tail work/rllab/nohup.out -n 300 |grep -v  trpo-expl_ '
+
 #ssh -i  /Users/lmj/seoul.pem ubuntu@dpl.ren "ssh -i digits.pem ubuntu@d.dpl.ren 'tail work/modular_rl-2p/nohup.out -n 5000 |grep RewP  ;  tail work/modular_rl-2p/nohup.out -n 90 |grep Ite   ; ps -ef|grep python|grep -v grep  ' "         
 
 
